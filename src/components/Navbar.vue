a6cbfd399acd22ce56fda1cb50e968b98036f41f
leaderdsn <leaderdsn.it@gmail.com>
2022-03-03 09:55:02 +0800

    created a navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="navbar">
+    <router-link to="/">Главная страница</router-link> |
+    <router-link to="/history">История</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
