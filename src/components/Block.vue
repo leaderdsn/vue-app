5dbe5430398044509513fd32117c1f45bf844262
leaderdsn <leaderdsn.it@gmail.com>
2022-03-03 09:58:17 +0800

    created a block

---
+++ src/components/Block.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="block">
+    <div class="block-header">
+      <slot name="b-header" :item="data"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue/types/options'
+interface ComplexItems {
+  id: string,
+  name: string,
+}
+export default {
+  name: 'Block',
+  props: {
+    data: {
+      type: Object as PropType<ComplexItems>,
+      default: ():PropType<ComplexItems> => []
+    }
+  },
+  methods: {}
+}
+</script>
