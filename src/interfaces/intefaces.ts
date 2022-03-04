dcb45e3b164b71dcb777ed6e1769c04c9fcc07ed
leaderdsn <leaderdsn.it@gmail.com>
2022-03-04 21:50:58 +0800

    created the "interface" file

---
+++ src/interfaces/intefaces.ts
@@ -0,0 +1,17 @@
+export interface IPost {
+  id: number
+  title: string
+  body?: string
+  userId?: number
+  type?: string
+  date?: string
+}
+export interface IDel {
+  idx: number
+  del: number
+}
+export interface IState {
+  history: IPost[],
+  desktop: IPost[],
+  basket: IPost[],
+}
