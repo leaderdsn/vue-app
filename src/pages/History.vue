d3a3a36097ecb9e2777af168e9616d573632ff43
leaderdsn <leaderdsn.it@gmail.com>
2022-03-05 18:38:49 +0800

    I made the history display depending on the query parameters

--- src/pages/History.vue
+++ src/pages/History.vue
@@ -9,25 +9,38 @@
         <span class="description">дата</span>
       </div>
     </div>
-    <div class="history-body" v-for="(item, idx) in HISTORY_DATA" :key="idx" >
-      <span>{{ ++idx }}</span>
-      <span>{{ item.type }}</span>
-      <span>{{ item.id }}</span>
-      <span>{{ item.title }}</span>
-      <span>{{ item.date }}</span>
+    <div v-if="HISTORY_DATA.length !== 0">
+      <div class="history-body" v-for="(item, idx) in historyFilter" :key="idx" >
+        <span>{{ ++idx }}</span>
+        <span>{{ item.type }}</span>
+        <span>{{ item.id }}</span>
+        <span>{{ item.title }}</span>
+        <span>{{ item.date }}</span>
+      </div>
+    </div>
+    <div v-else>
+      <h1>Записей в истории нет</h1>
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { mapGetters } from 'vuex'
+import { IPost } from '@/interfaces/intefaces'
 export default {
   name: 'History',
   computed: {
-    ...mapGetters([
-      'COUNT',
-      'HISTORY_DATA'
-    ])
+    ...mapGetters(['HISTORY_DATA']),
+    historyFilter: function ():IPost[] {
+      if (this.$route.query.filter === 'added') {
+        return this.HISTORY_DATA.filter((item: { type: string }) => item.type === 'Добавление')
+      } else
+      if (this.$route.query.filter === 'deletion') {
+        return this.HISTORY_DATA.filter((item: { type: string }) => item.type === 'Удаление')
+      } else {
+        return this.HISTORY_DATA
+      }
+    }
   }
 }
 </script>
